0c626a9883fca27f09a4f7346bef97ce400cde76
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-03-23 13:21:55 +0100

    update: nesa media page

--- app/(main)/nesa-media/page.tsx
+++ app/(main)/nesa-media/page.tsx
@@ -1,12 +1,13 @@
 import { raleway } from "@/lib/utils/font";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
 
 const Page = () => {
   return (
     <>
-      <header className="md:min-h-[25rem] min-h-screen relative">
+      <header className="md:min-h-[35rem] min-h-screen relative">
         <Image
-          src={"/images/bg/team_bg.jpg"}
+          src={"/images/bg/media-bg.jpg"}
           alt={"background"}
           width={1024}
           height={800}
@@ -35,12 +36,12 @@ const Page = () => {
             <p className="font-bold text-deepGold">SEE MORE</p>
           </div>
 
-          <div className="mt-2 space-y-8">
-            {Array.from({ length: 4 }).map((_, id) => (
-              <div key={id} className="grid md:grid-cols-2 items-center bg-[#fff5e0]">
+          <div className="mt-2 grid md:grid-cols-3 gap-2">
+            {Array.from({ length: 6 }).map((_, id) => (
+              <div key={id} className="grid items-center bg-[#fff5e0] rounded-lg overflow-hidden">
                 <div className="md:max-h-[25rem] max-h-[18rem] overflow-hidden">
                   <Image
-                    src={`/images/media/${id + 1}.png`}
+                    src={`/images/media/${id >= 4 ? 1 : id + 1}.png`}
                     alt="image"
                     width={500}
                     height={500}
@@ -52,7 +53,7 @@ const Page = () => {
                     CATEGORY {id + 1}: <br />
                     NGO Contributions to Education{" "}
                   </p>
-                  <p>
+                  <p className="line-clamp-5">
                     The NGO Educational Champion of the Decade Award is established to honor and recognize the pivotal
                     role of Nigerian indigenous NGOs and civil society organizations in championing the cause of
                     &apos;Education for All&apos;. The 2023 award cycle specifically acknowledges those entities that
@@ -72,7 +73,10 @@ const Page = () => {
 
             <div className="grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 md:gap-20 gap-12">
               {Array.from({ length: 6 }).map((_, id) => (
-                <div key={id} className="items-center space-y-2">
+                <div
+                  key={id}
+                  className="items-center space-y-2 cursor-pointer duration-200 hover:-translate-y-2 bg-darkGold rounded-md overflow-hidden"
+                >
                   <div className="md:max-h-[15rem] max-h-[10rem] overflow-hidden">
                     <Image
                       src={`/images/media/${id >= 4 ? 1 : id + 1}.png`}
@@ -82,19 +86,29 @@ const Page = () => {
                       className="w-full h-full object-cover"
                     />
                   </div>
-                  <p>NIGERIA: THE GIANT OF AFRICA DOCUMENTARY</p>
+                  <div className="p-2">
+                    <p className="text-center">NIGERIA: THE GIANT OF AFRICA DOCUMENTARY</p>
+                  </div>
                 </div>
               ))}
             </div>
           </div>
         </div>
 
-        <div className="container mt-16 space-y-8">
-          <h3 className="font-bold text-3xl">MOMENTS IN PICTURES</h3>
-          <div className="grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:grid-cols-2 lg:gap-10 md:gap-20 gap-12">
-            {Array.from({ length: 20 }).map((_, id) => (
-              <div key={id} className="items-center space-y-2">
-                <div className="md:max-h-[15rem] max-h-[10rem] overflow-hidden">
+        <div className="container mt-16 space-y-8 relative">
+          <h3 className="font-extrabold text-3xl text-center">MOMENTS IN PICTURES</h3>
+
+          {/* left arrow */}
+          <ChevronLeft className="cursor-pointer text-deepGold absolute top-[44%] -left-8" size={50} />
+
+          {/* slides */}
+          <div className="grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 lg:gap-10 md:gap-20 gap-12">
+            {Array.from({ length: 4 }).map((_, id) => (
+              <div
+                key={id}
+                className="items-center space-y-2 duration-200 group ring ring-darkGold/10 rounded-md overflow-hidden hover:ring-deepGold"
+              >
+                <div className="md:max-h-[15rem] max-h-[10rem] overflow-hidden rounded-md">
                   <Image
                     src={`/images/media/${
                       id >= 4 ? [1, 2, 3, 4][Math.floor(Math.random() * [1, 2, 3, 4].length)] : id + 1
@@ -102,10 +116,25 @@ const Page = () => {
                     alt="image"
                     width={500}
                     height={500}
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-cover group-hover:scale-105 duration-200"
                   />
                 </div>
               </div>
+            ))}
+          </div>
+
+          {/* right arrow */}
+          <ChevronRight className="cursor-pointer text-deepGold absolute top-[44%] -right-8" size={50} />
+
+          {/* indicators */}
+          <div className="flex items-center gap-2 justify-center mt-5">
+            {Array.from({ length: 4 }).map((_, id) => (
+              <div
+                key={id}
+                className={`w-2 h-2 rounded-full duration-200 border ${
+                  id === 2 ? "bg-deepGold border-deepGold px-3" : "bg-transparent border-darkGold/50"
+                }`}
+              ></div>
             ))}
           </div>
         </div>
