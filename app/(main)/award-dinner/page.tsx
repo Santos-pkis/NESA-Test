061cbca870dce787b74e9ede3ea3286d3138273b
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-03-23 13:33:59 +0100

    update: award dinner page

--- app/(main)/award-dinner/page.tsx
+++ app/(main)/award-dinner/page.tsx
@@ -3,140 +3,13 @@
 import SantosLink from "@/components/Common/Links/santos";
 import { additionalOpportunities, benefits, sponsors } from "@/lib/data/others";
 import { useModal } from "@/lib/store/modal";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { raleway } from "@/lib/utils/font";
 import { ExternalLink } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { step1Variant, step2Variant } from "@/components/Common/Others/get-involved";
 import Modal from "@/components/Common/Modal";
-import { useRouter } from "next/navigation";
-
-const SponsorshipModalContent = () => {
-  const [step, setStep] = useState(1);
-
-  const router = useRouter();
-
-  const { hideModal } = useModal();
-  const handleNext = () => {
-    if (step < 2) {
-      setStep((prev) => prev + 1);
-      return;
-    }
-
-    toast.success("Submitted Successfully 🎉");
-    hideModal();
-    router.replace("/");
-  };
-
-  const renderForm = (): JSX.Element | null => {
-    switch (step) {
-      case 1:
-        return (
-          <motion.div {...step1Variant} className="space-y-4 max-h-[40rem] overflow-y-auto" key={"step-1"}>
-            <div className="p-1">
-              <p className="font-bold text-xl text-midGold">Partner with us</p>
-              <p className="text-sm text-zinc-600">Fill in the required details and get to partner with us</p>
-            </div>
-            <div className="space-y-1">
-              <label htmlFor="fullName" className="font-medium text-sm">
-                Full Name
-              </label>
-              <input
-                type="text"
-                name="fullName"
-                className="w-full outline-none p-2 rounded-md border border-zinc-300"
-                placeholder="John"
-              />
-            </div>
-            <div className="space-y-1">
-              <label htmlFor="company" className="font-medium text-sm">
-                Company Name (if any)
-              </label>
-              <input
-                type="text"
-                name="company"
-                className="w-full outline-none p-2 rounded-md border border-zinc-300"
-                placeholder="Doe"
-              />
-            </div>
-            <div className="space-y-1">
-              <label htmlFor="email" className="font-medium text-sm">
-                Email Address
-              </label>
-              <input
-                type="email"
-                name="email"
-                className="w-full outline-none p-2 rounded-md border border-zinc-300"
-                placeholder="example@gmail.com"
-              />
-            </div>
-            <div className="space-y-1">
-              <label htmlFor="phoneNumber" className="font-medium text-sm">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                name="phoneNumber"
-                className="w-full outline-none p-2 rounded-md border border-zinc-300"
-                placeholder="+234 901223456789"
-              />
-            </div>
-
-            <div className="space-y-1">
-              <label htmlFor="country" className="font-medium text-sm">
-                Country
-              </label>
-              <select name="country" className="w-full outline-none p-2 rounded-md border border-zinc-300">
-                <option value="nigeria">Nigeria</option>
-              </select>
-            </div>
-            <div className="space-y-1">
-              <label htmlFor="areaOfSponsorship" className="font-medium text-sm">
-                Area of Sponsorship
-              </label>
-              <select name="country" className="w-full outline-none p-2 rounded-md border border-zinc-300">
-                <option value="nigeria">Option1</option>
-                <option value="nigeria">Option2</option>
-              </select>
-            </div>
-
-            <button className="w-full text-darkGold py-2 bg-deepGold rounded-lg" onClick={handleNext}>
-              Submit
-            </button>
-          </motion.div>
-        );
-      case 2:
-        return (
-          <motion.div {...step2Variant} className="space-y-4 max-h-[40rem] overflow-y-auto" key={"step-2"}>
-            <div className="text-center space-y-3">
-              <p className="text-lg text-midGold font-bold">Thanks for been part of a Noble Cause!</p>
-              <p className="text-sm max-w-md mx-auto text-zinc-500">
-                Your response has been recorded and the Sponsorship Team would reach out to you soon
-              </p>
-              <button className="w-full text-darkGold py-2 bg-deepGold rounded-lg" onClick={handleNext}>
-                Back to Home
-              </button>
-            </div>
-          </motion.div>
-        );
-      default:
-        return null;
-    }
-  };
-
-  return (
-    <div className="bg-white p-40 rounded-lg px-5 py-8 md:min-w-[30rem] max-h-[40rem] overflow-y-auto overflow-x-hidden">
-      <div className="space-y-4 max-h-[40rem] overflow-y-auto">
-        <AnimatePresence mode="wait" initial={false}>
-          {renderForm()}
-        </AnimatePresence>
-      </div>
-    </div>
-  );
-};
+import SponsorshipModalContent from "./modal";
 
 const Page = () => {
   const { showModal, visible, hideModal } = useModal();
@@ -158,13 +31,14 @@ const Page = () => {
           </p>
         </div>
       </header>
+
       <main>
-        <div className="my-16 grid md:grid-cols-2 gap-10 items-center container">
-          <div className="flex flex-col gap-6 self-start">
-            <div className="p-6 font-bold md:text-3xl text-2xl from-[#ffd174] bg-gradient-to-r">
+        <div className="md:my-24 my-16 md:grid grid-cols-2 flex flex-col-reverse md:gap-10 gap-6 items-center container">
+          <div className="md:space-y-10 space-y-6 self-start">
+            <div className="p-6 font-bold md:text-3xl text-2xl rounded-md from-[#ffd174] bg-gradient-to-r">
               <p>Welcome to the New Education Standard Award Nigeria (NESA) 2023 Award Dinner Ceremony Page!</p>
             </div>
-            <div className="p-6 from-[#d5ff7c] to-[#f3ffd8] bg-gradient-to-r">
+            <div className="p-6 from-[#d5ff7c] to-[#f3ffd8] bg-gradient-to-r rounded-md">
               <p>
                 This year, we celebrate a milestone in educational transformation with the NESA 2023 Award Dinner, an
                 evening dedicated to honoring the champions of education in Nigeria. Our prestigious event recognizes
@@ -173,8 +47,14 @@ const Page = () => {
               </p>
             </div>
           </div>
-          <div>
-            <Image src={"/images/award-dinner.png"} alt="award dinner" className="" width={400} height={800} />
+          <div className="md:min-h-[30rem] min-h-[25rem] relative overflow-hidden rounded-md w-full">
+            <Image
+              src={"/images/bg/award-dinner.jpg"}
+              alt="award dinner"
+              className="w-full h-full object-cover absolute top-0 left-0"
+              width={800}
+              height={800}
+            />
           </div>
         </div>
 
@@ -244,7 +124,7 @@ const Page = () => {
           </div>
           <div className="md:col-span-2 max-h-[40rem] overflow-hidden">
             <Image
-              src={"/images/award-dinner.png"}
+              src={"/images/bg/award-dinner.jpg"}
               alt="award dinner"
               className="w-full h-full object-cover"
               width={400}
