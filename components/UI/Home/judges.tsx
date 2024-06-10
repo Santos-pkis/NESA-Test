98535e90db8f212e2f5a683c2a34ba2012d83b50
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-06-10 12:17:28 +0100

    update: judges section (home)

--- components/UI/Home/judges.tsx
+++ components/UI/Home/judges.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Button from "@/components/Common/Button";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import Image from "next/image";
 
 const Judges = () => {
   return (
     <div>
-      <div className="min-h-[40rem] relative flex items-center">
+      <div className="min-h-[50rem] relative flex items-center">
         <Image
           src={"/images/bg/timeline.png"}
           alt="dark background"
@@ -14,10 +16,18 @@ const Judges = () => {
           height={600}
         />
 
-        <div className="container text-white space-y-6">
+        <div className="container text-white space-y-16 py-10">
           <div className="space-y-2">
             <h3 className="_under_border font-bold text-2xl">Meet The Judges</h3>
             <p className="text-2xl">Get to know the NESA Judges, their background, and acheivements</p>
+          </div>
+
+          <div className="flex items-center justify-between flex-wrap">
+            {Array.from({ length: 3 }).map((_, id) => (
+              <div key={id}>
+                <Image src={`/images/j${id + 1}.png`} alt={`Judge ${id + 1}`} width={300} height={400} />
+              </div>
+            ))}
           </div>
 
           <div className="flex items-center justify-between">
@@ -28,6 +38,14 @@ const Judges = () => {
                   className={`w-3 h-3 rounded-full bg-white cursor-pointer ${id === 0 ? "bg-primaryGold" : ""}`}
                 ></div>
               ))}
+            </div>
+            <div className="flex items-center space-x-2 text-black">
+              <button className="size-12 rounded-xl bg-white grid place-content-center">
+                <ArrowLeft />
+              </button>
+              <button className="size-12 rounded-xl bg-primaryGold grid place-content-center">
+                <ArrowRight />
+              </button>
             </div>
           </div>
         </div>
