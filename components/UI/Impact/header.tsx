f984bf692e6895444d4852e7118c92a3a0be4ca7
Prime <wolabash@gmail.com>
2024-07-12 06:45:03 +0100

    Feat: fix build error

--- components/UI/Impact/header.tsx
+++ components/UI/Impact/header.tsx
@@ -22,28 +22,38 @@ const ImpactHeader = () => {
 
         <div className="w-full h-full flex items-center absolute top-0 left-0 pt-12">
           <div className="container">
-            <motion.div variants={parentV} initial="initial" animate="animate" className="space-y-6 text-center py-8">
+            <motion.div
+              variants={parentV}
+              initial="initial"
+              animate="animate"
+              className="space-y-6 text-center py-8"
+            >
               <motion.div variants={opacityV} className="space-y-2">
-                <h1 className="font-bold md:text-5xl text-4xl text-midGold md:leading-[1.4]">IMPACT @ NESA AFRICA</h1>
+                <h1 className="font-bold md:text-5xl text-4xl text-midGold md:leading-[1.4]">
+                  IMPACT @ NESA AFRICA
+                </h1>
                 <p className="max-w-2xl mx-auto text-center">
-                  In a country facing infrastructural deficits, outdated curricula, and educator motivation issues, NESA
-                  Nigeria strives to encourage government bodies, NGOs, UN agencies, CSR initiatives, and private
-                  entities to play pivotal roles in improving education
+                  In a country facing infrastructural deficits, outdated
+                  curricula, and educator motivation issues, NESA Nigeria
+                  strives to encourage government bodies, NGOs, UN agencies, CSR
+                  initiatives, and private entities to play pivotal roles in
+                  improving education
                 </p>
               </motion.div>
-             <div className="h-[520px] w-[1280px] flex justify-center mx-auto">
-              <video
-                src={"/images/about.mp4"}
-                width={1280}
-                height={500}
-                autoPlay
-                loop
-                muted={false}
-                controls
-                playsInline
-                className="w-full h-auto max-w-[1280px] mx-auto  object-cover"
-              ></video>
-            </div>
+              <div className="h-[520px] w-[1280px] flex justify-center mx-auto">
+                <video
+                  src={"/images/about.mp4"}
+                  width={1280}
+                  height={500}
+                  autoPlay
+                  loop
+                  muted={false}
+                  controls
+                  playsInline
+                  className="w-full h-auto max-w-[1280px] mx-auto  object-cover"
+                ></video>
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
