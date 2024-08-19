95b7196f29eadd3c1b0f2bb9f657471dc51897cd
DAMMYTJ <dammmytijani@gmail.com>
2024-08-19 11:53:59 +0100

    new build

---
+++ dist/_next/static/chunks/app/(account)/layout-409926a27e9ac517.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9532,6695,1198,1896,1703,934],{3521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g7d1Pp55JZ2EO+HoFgVvpO2tsV8QfRecAf56C7j5wRY=\"\n}"
