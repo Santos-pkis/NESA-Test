97ad4fe33d51e3bcee94de875ac2510010c5cf3b
chimebukanian <chimebukaanyanwu@gmail.com>
2025-05-07 15:09:24 +0100

    competitive-refactored

--- components/UI/Home/competitive-hero-center.tsx
+++ components/UI/Home/competitive-hero-center.tsx
@@ -49,9 +49,9 @@ const CompetitiveHeroCenter = ({ index, title, description, subCategoryPath }: P
             <div className="flex pt-1 items-center px-1 gap-1 md:gap-3 justify-center md:justify-start">
               <Button
               text="View Categories"
-              className="flex-grow rounded-md  md:px-8 py-2 text-center hover:bg-[#E48900] hover:text-black  sm:w-auto text-sm md:text-base"
+              className=" rounded-md  md:px-8 py-2 text-center hover:bg-[#E48900] hover:text-black  sm:w-auto w-1/2 text-sm md:text-base"
               />
-              <Button text="Nominate Now" variant="filled" className="flex-grow rounded-md md:px-8 py-2 text-center hover:bg-[#E48900] hover:text-black  w-auto" />
+              <Button text="Nominate Now" variant="filled" className=" rounded-md md:px-8 py-2 text-center w-1/2 hover:bg-[#E48900] hover:text-black  w-auto" />
             </div>
           </div>
         
